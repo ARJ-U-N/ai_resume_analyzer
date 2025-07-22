102bddde1eb1d4580c11e75493694c081218daf9
ARJ-U-N <ghub70242@gmail.com>
2025-07-22 21:41:33 +0530

    complete

---
+++ app/routes/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </main>
+  );
+}
